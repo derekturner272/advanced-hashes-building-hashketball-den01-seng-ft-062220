f89be7188fe60a1360f5a94ab0e662ea2d81b45f
Derek Turner <turner.derek.b@gmail.com>
2020-06-15 18:22:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,6 +2,20 @@
 def game_hash
   game_hash = {
     :home => {
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black","White"],
+      :players => [
+        {:player_name => ""},
+        {:number => },
+        {:shoe => },
+        {:points => },
+        {:rebounds => },
+        {:assists => },
+        {:steals => },
+        {:blocks =>},
+        {:slam_dunks => },
+        ]},
+    :away => {
       :team_name => "",
       :colors => ["",""],
       :players => [
