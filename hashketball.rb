2f61d7f962a0da6cbb01924c1101a47e0fe962d9
Derek Turner <turner.derek.b@gmail.com>
2020-06-15 18:15:13 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,7 +1,15 @@
 # Write your code here!
 def game_hash
   game_hash = {
-    
+    :home => {
+      :team_name => "",
+      :colors => ["",""],
+      :players => [
+        {:player_name => ""},
+        {},
+        ]
+      
+    }
     
   }
   
